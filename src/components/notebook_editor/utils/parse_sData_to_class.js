c584f58c4b489d03a3141913e16634d15a8fecae
Porti <47105148+PortiESP@users.noreply.github.com>
2025-01-14 02:17:48 +0100

    fix: cache untitled sections

--- src/components/notebook_editor/utils/parse_sData_to_class.js
+++ src/components/notebook_editor/utils/parse_sData_to_class.js
@@ -1,10 +1,19 @@
 import Blank from "../components/section/section_types/blank/blank_class"
+import Default from "../components/section/section_types/default/default_class"
+import Gap from "../components/section/section_types/gap/gap_class"
 import { deepClone } from "./clone"
 import { private2public } from "./general"
 import parseElementsDataToClassObject from "./parse_eData_to_class"
 
 export default function parseSDataToClass(sData) {
     const templateSection = private2public(deepClone(sData))
+
+    if (templateSection.type === "gap") {
+        return new Gap({ id: templateSection.id, height: templateSection.height, title: templateSection.title, elements: parseElementsDataToClassObject(templateSection.elements) })
+    } else if (templateSection.type === "default") {
+        return new Default({ id: templateSection.id, height: templateSection.height, title: templateSection.title, elements: parseElementsDataToClassObject(templateSection.elements) })
+    }
+
     const newSection = new Blank({ id: templateSection.id, height: templateSection.height, title: templateSection.title, elements: parseElementsDataToClassObject(templateSection.elements) })
     return newSection
 }
